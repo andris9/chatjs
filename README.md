0205c43b744781dd8c37e1892f0c65a4bf01ad8d
Andris Reinman <andris@reinman.eu>
2023-08-07 14:47:09 +0300

    Added installer

---
+++ README.md
@@ -0,0 +1,27 @@
+# chatjs
+
+Demo chat running on Llama2
+
+### 1. Setup
+
+Run the installer (tested on Linux and MacOS)
+
+```
+$ ./install.sh
+```
+
+> The install script compiles the llama2.cpp application and downloads the Llama2 13B chat model, so it is going to take some time and make sure your system has a compiler available
+
+### 2. Run the app
+
+```
+$ npm start
+```
+
+## Usage
+
+Open browser at [http://127.0.0.1:3000](http://127.0.0.1:3000) and start chatting!
+
+## License
+
+**ISC**
---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+if ! command -v make &> /dev/null
+then
+    echo "Compiler is missing"
+    exit 1
+fi
+
+rm -rf llama.cpp
+git clone https://github.com/ggerganov/llama.cpp.git
+cd llama.cpp
+make clean
+
+if [ "$machine" == "Mac" ]; then
+    LLAMA_METAL=1 make
+else
+    make
+fi
+
+export MODEL=llama-2-13b-chat.ggmlv3.q4_0.bin
+wget "https://huggingface.co/TheBloke/Llama-2-13B-chat-GGML/resolve/main/${MODEL}"
+
+# Test
+
+./main \
+    --threads 4 \
+    --n-gpu-layers 1 \
+    --model ${MODEL} \
+    --color \
+    --ctx-size 2048 \
+    --temp 0.7 \
+    --repeat_penalty 1.1 \
+    --n-predict -1 \
+    --prompt "[INST] Tell me a joke [/INST]"
+
+echo "If you see a joke (even a bad one) then it means the process completed successfully
